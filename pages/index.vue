d7c9c2dc50e7aa60bdd08cbffacf4f1bf178eb66
Shalini <shalini5640j@gmail.com>
2023-09-17 11:33:50 +0530

    add navigation button

--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,11 @@
     </h2>
 
     <img h-8 src="https://6778953.fs1.hubspotusercontent-na1.net/hubfs/6778953/Brand/Circle/circle-logo-color.png" alt="">
+
+    <NuxtLink to="/store" class="text-sm" mt-4 flex items-center gap-2 rounded-md bg-indigo px-4 py-2>
+      Continue to Store
+      <div i-material-symbols-line-end-arrow />
+    </NuxtLink>
   </div>
 </template>
 
