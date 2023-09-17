e0efed0df4a6ef438c0732c70fe1eef159fb5d64
Shalini <shalini5640j@gmail.com>
2023-09-17 11:29:30 +0530

    add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+
+const circleBalance = await $client.circle.getBalance.query()
+</script>
+
+<template>
+  <div flex items-center justify-between>
+    <div flex items-center gap-4>
+      <h1 font-black tracking-wider uppercase text-blue>
+        Equi-Store
+      </h1>
+    </div>
+    <div flex items-center gap-2>
+      <div font-bold tracking-wide font-mono text-indigo>
+        Circle Funds Stats:
+      </div>
+      <div rounded-sm bg-white px-2 py-1 text-sm font-mono text-black shadow-md>
+        Available: {{ circleBalance?.available.length }}
+      </div>
+
+      <div rounded-sm bg-white px-2 py-1 text-sm font-mono text-black shadow-md>
+        Unsettled: {{ circleBalance?.unsettled.length }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
