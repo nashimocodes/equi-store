746a98fb8b72155fbb54d61256370beba70ba522
Shalini <shalini5640j@gmail.com>
2023-09-17 11:29:35 +0530

    layout and store page

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  <div>
+  <div flex="~ col" bg-stretch h-screen gap-4 overflow-y-hidden bg-zinc-900 bg-cover bg-no-repeat px-4 py-4 text-white>
+    <Navbar />
+
     <slot />
   </div>
 </template>
