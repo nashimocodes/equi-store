608e6d7a683011f55ace7b118fad3903a75ade92
Shalini <shalini5640j@gmail.com>
2023-09-17 14:36:03 +0530

    circle backend processing

--- server/trpc/routers/circle.ts
+++ server/trpc/routers/circle.ts
@@ -1,6 +1,12 @@
+import { Currency, PaymentCreationRequestVerificationEnum, SourceTypeEnum } from '@circle-fin/circle-sdk'
 import { z } from 'zod'
+import { v4 } from 'uuid'
+import { faker } from '@faker-js/faker'
+import { nanoid } from 'nanoid'
+import { TRPCError } from '@trpc/server'
 import { publicProcedure, router } from '../trpc'
 import { circle } from '~/lib/payments'
+import { encryptCard } from '~/server/utils/circle'
 
 export const schema = z.object({
   number: z.string(),
@@ -29,7 +35,64 @@ export const circleRouter = router({
   createPayment: publicProcedure
     .input(schema)
     .mutation(async ({ input }) => {
-      return 'Hello'
+      const { number, cvv, expMonth, expYear, billingDetails, amount } = input
+
+      const encryptedMessage = await encryptCard({
+        number,
+        cvv,
+      })
+
+      const { keyId } = (await circle.encryption.getPublicKey()).data.data!
+      const metadata = {
+        email: faker.internet.email({
+          provider: 'nekocandy.club',
+        }),
+        sessionId: nanoid(),
+        ipAddress: '1.1.1.1',
+      }
+
+      const cardDetails = {
+        idempotencyKey: v4(),
+        encryptedData: encryptedMessage,
+        billingDetails,
+        keyId,
+        expMonth: Number(expMonth),
+        expYear: Number(expYear),
+        metadata,
+      }
+
+      try {
+        const data = await circle.cards.createCard(cardDetails)
+        const cardId = data.data.data?.id
+
+        const paymentDetails = {
+          idempotencyKey: v4(),
+          metadata,
+          amount: {
+            amount: amount.toString(),
+            currency: Currency.Usd,
+          },
+          autoCapture: true,
+          verification: PaymentCreationRequestVerificationEnum.Cvv,
+          source: {
+            id: cardId,
+            type: SourceTypeEnum.Card,
+          },
+          description: 'Payment for Lancing Order',
+        }
+
+        const createPaymentResponse = await circle.payments.createPayment(
+          paymentDetails,
+        )
+
+        return createPaymentResponse.data.data!
+      }
+      catch (error: any) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: `We\'re having trouble processing your payment. Please try again later.\n\nStack:\n${error.stack}`,
+        })
+      }
     }),
 
 })
