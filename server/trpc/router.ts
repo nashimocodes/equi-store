620ec57cba643a949649d8a72878e5cc73b5246c
Shalini <shalini5640j@gmail.com>
2023-09-17 11:29:18 +0530

    add circle router

--- server/trpc/router.ts
+++ server/trpc/router.ts
@@ -1,6 +1,8 @@
 import { router } from './trpc'
+import { circleRouter } from '~/server/trpc/routers/circle'
 
 export const appRouter = router({
+  circle: circleRouter,
 })
 
 export type AppRouter = typeof appRouter
--- server/trpc/routers/circle.ts
+++ server/trpc/routers/circle.ts
@@ -1,7 +1,7 @@
 import { publicProcedure, router } from '../trpc'
 import { circle } from '~/lib/payments'
 
-export const deedsRouter = router({
+export const circleRouter = router({
   getBalance: publicProcedure
     .query(async () => {
       const balances = await circle.balances.listBalances()
